385647adb7c766a589548c98017a7c729b303806
Adrian Riedl <adrian.riedl@tum.de>
2020-01-22 22:43:09 +0100

    Fixing build error due to __attribute__

--- src/execution/operator/join/physical_radix_join.cpp
+++ src/execution/operator/join/physical_radix_join.cpp
@@ -564,10 +564,9 @@ PhysicalRadixJoin::FillPartitions(PhysicalRadixJoinOperatorState *state, std::at
     }
 }
 
-void __attribute__((always_inline))
-PhysicalRadixJoin::RadixJoinPartitionWorkerLeft(PhysicalRadixJoinOperatorState *state, index_t startOfPartitions,
-                                                index_t endOfPartitions, size_t bitmask, size_t bitMaskNextRun,
-                                                size_t shift, size_t run, size_t partitionNumber) {
+void PhysicalRadixJoin::RadixJoinPartitionWorkerLeft(PhysicalRadixJoinOperatorState *state, index_t startOfPartitions,
+                                                     index_t endOfPartitions, size_t bitmask, size_t bitMaskNextRun,
+                                                     size_t shift, size_t run, size_t partitionNumber) {
     // Iterate over the data given to this thread
     for (index_t toOrder = startOfPartitions; toOrder < endOfPartitions; toOrder++) {
         auto values = state->left_data->GetRow(toOrder);
@@ -592,10 +591,9 @@ PhysicalRadixJoin::RadixJoinPartitionWorkerLeft(PhysicalRadixJoinOperatorState *
     }
 }
 
-void __attribute__((always_inline))
-PhysicalRadixJoin::RadixJoinPartitionWorkerRight(PhysicalRadixJoinOperatorState *state, index_t startOfPartitions,
-                                                 index_t endOfPartitions, size_t bitmask, size_t bitMaskNextRun,
-                                                 size_t shift, size_t run, size_t partitionNumber) {
+void PhysicalRadixJoin::RadixJoinPartitionWorkerRight(PhysicalRadixJoinOperatorState *state, index_t startOfPartitions,
+                                                      index_t endOfPartitions, size_t bitmask, size_t bitMaskNextRun,
+                                                      size_t shift, size_t run, size_t partitionNumber) {
     for (index_t toOrder = startOfPartitions; toOrder < endOfPartitions; toOrder++) {
         auto values = state->right_data->GetRow(toOrder);
         auto hash = values[values.size() - 1].value_.hash;
