540976c8d73f2360f36f9e232c8c6d22ac93a386
Adrian Riedl <adrian.riedl@tum.de>
2020-01-22 22:27:17 +0100

    Adapt number of tuples

--- Benchmarks/RadixJoin/makePlotsRadix.py
+++ Benchmarks/RadixJoin/makePlotsRadix.py
@@ -7,7 +7,7 @@ HEADINGSTIMER = ["Tuples", "CollLeft","PartLeft", "CollRight", "PartRight", "Set
 HEADINGSNOTIMER = ["Tuples", "runtime"]
 
 START = 6
-END = 24
+END = 21
 
 def power2(ex):
     if ex == 0:
